921093eede8ac9b2f2a45c25e7ff01b4b703fa43
LuisGarcia22550366 <124327925+LuisGarcia22550366@users.noreply.github.com>
2023-03-24 08:53:10 -0700

    Add files via upload

---
+++ Eva_2_Interfaces_Figuras/src/eva_2_interfaces_figuras/Eva_2_Interfaces_Figuras.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2_interfaces_figuras;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva_2_Interfaces_Figuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Circle c = new Circle(20);
+        c.printData();
+        Triangle t = new Triangle(20, 20);
+        t.printData();
+    }
+    
+}
---
+++ Eva_2_Interfaces_Figuras/src/eva_2_interfaces_figuras/Figuras.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2_interfaces_figuras;
+
+/**
+ *
+ * @author invitado
+ */
+public interface Figuras {
+    
+    double cArea();
+    double cPerimetro();
+    
+}
